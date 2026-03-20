1524c9a090b61235649557095c1cd0517fa05530
howardcallanta00-ship-it <howard.callanta00@gmail.com>
2026-03-20 16:16:17 +0800

    Add gitignore, fix dependencies, fix port

--- server.js
+++ server.js
@@ -22,6 +22,8 @@ app.get("/", (req, res) => {
   res.send("Ebookstore API is running...");
 });
 
-app.listen(5000, () => {
-  console.log("Backend running at http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Backend running on port ${PORT}`);
 });
